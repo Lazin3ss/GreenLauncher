7a91d12f5e1ba27f02e23a2ecb6b89c78bd96162
Lazin3ss <laziness@protonmail.com>
2025-01-13 03:52:21 -0300

    Improved add/edit dialog, remove unnecessary files

--- include/EditGame.h
+++ include/EditGame.h
@@ -11,13 +11,11 @@
 #define EDITGAME_H
 
 //(*Headers(EditGame)
-#include <wx/button.h>
-#include <wx/checkbox.h>
+#include <wx/choice.h>
 #include <wx/dialog.h>
+#include <wx/notebook.h>
 #include <wx/panel.h>
 #include <wx/sizer.h>
-#include <wx/stattext.h>
-#include <wx/textctrl.h>
 //*)
 
 class EditGame: public wxDialog
@@ -28,61 +26,31 @@ class EditGame: public wxDialog
         virtual ~EditGame();
 
         //(*Declarations(EditGame)
-        wxButton* Button1;
-        wxButton* Button2;
-        wxCheckBox* CheckBox1;
+        wxChoice* Choice1;
+        wxNotebook* Notebook1;
         wxPanel* Panel1;
-        wxStaticText* StaticText1;
-        wxStaticText* StaticText2;
-        wxStaticText* StaticText3;
-        wxStaticText* StaticText4;
-        wxStaticText* StaticText5;
-        wxStaticText* StaticText6;
-        wxStaticText* StaticText7;
-        wxStaticText* StaticText8;
-        wxStaticText* StaticText9;
-        wxTextCtrl* TextCtrl1;
-        wxTextCtrl* TextCtrl2;
-        wxTextCtrl* TextCtrl3;
-        wxTextCtrl* TextCtrl4;
-        wxTextCtrl* TextCtrl5;
-        wxTextCtrl* TextCtrl6;
-        wxTextCtrl* TextCtrl7;
-        wxTextCtrl* TextCtrl8;
+        wxPanel* Panel2;
         //*)
 
     protected:
 
         //(*Identifiers(EditGame)
-        static const long ID_STATICTEXT1;
-        static const long ID_TEXTCTRL1;
-        static const long ID_STATICTEXT2;
-        static const long ID_TEXTCTRL2;
-        static const long ID_STATICTEXT3;
-        static const long ID_TEXTCTRL3;
-        static const long ID_STATICTEXT4;
-        static const long ID_TEXTCTRL4;
-        static const long ID_STATICTEXT5;
-        static const long ID_TEXTCTRL5;
-        static const long ID_STATICTEXT6;
-        static const long ID_TEXTCTRL6;
-        static const long ID_STATICTEXT7;
-        static const long ID_TEXTCTRL7;
-        static const long ID_STATICTEXT8;
-        static const long ID_TEXTCTRL8;
-        static const long ID_STATICTEXT9;
-        static const long ID_CHECKBOX1;
-        static const long ID_BUTTON1;
-        static const long ID_BUTTON2;
+        static const long ID_CHOICE1;
         static const long ID_PANEL1;
+        static const long ID_PANEL2;
+        static const long ID_NOTEBOOK1;
         //*)
 
     private:
 
         //(*Handlers(EditGame)
+        void OnNotebook1PageChanged(wxNotebookEvent& event);
+        void OnPanel1Paint(wxPaintEvent& event);
+        void OnNotebook1PageChanged1(wxNotebookEvent& event);
         //*)
 
         DECLARE_EVENT_TABLE()
 };
 
 #endif
+
--- src/EditGame.cpp
+++ src/EditGame.cpp
@@ -10,32 +10,15 @@
 #include "EditGame.h"
 
 //(*InternalHeaders(EditGame)
+#include <wx/button.h>
 #include <wx/intl.h>
 #include <wx/string.h>
 //*)
 
 //(*IdInit(EditGame)
-const long EditGame::ID_STATICTEXT1 = wxNewId();
-const long EditGame::ID_TEXTCTRL1 = wxNewId();
-const long EditGame::ID_STATICTEXT2 = wxNewId();
-const long EditGame::ID_TEXTCTRL2 = wxNewId();
-const long EditGame::ID_STATICTEXT3 = wxNewId();
-const long EditGame::ID_TEXTCTRL3 = wxNewId();
-const long EditGame::ID_STATICTEXT4 = wxNewId();
-const long EditGame::ID_TEXTCTRL4 = wxNewId();
-const long EditGame::ID_STATICTEXT5 = wxNewId();
-const long EditGame::ID_TEXTCTRL5 = wxNewId();
-const long EditGame::ID_STATICTEXT6 = wxNewId();
-const long EditGame::ID_TEXTCTRL6 = wxNewId();
-const long EditGame::ID_STATICTEXT7 = wxNewId();
-const long EditGame::ID_TEXTCTRL7 = wxNewId();
-const long EditGame::ID_STATICTEXT8 = wxNewId();
-const long EditGame::ID_TEXTCTRL8 = wxNewId();
-const long EditGame::ID_STATICTEXT9 = wxNewId();
-const long EditGame::ID_CHECKBOX1 = wxNewId();
-const long EditGame::ID_BUTTON1 = wxNewId();
-const long EditGame::ID_BUTTON2 = wxNewId();
 const long EditGame::ID_PANEL1 = wxNewId();
+const long EditGame::ID_PANEL2 = wxNewId();
+const long EditGame::ID_NOTEBOOK1 = wxNewId();
 //*)
 
 BEGIN_EVENT_TABLE(EditGame,wxDialog)
@@ -46,58 +29,32 @@ END_EVENT_TABLE()
 EditGame::EditGame(wxWindow* parent,wxWindowID id)
 {
     //(*Initialize(EditGame)
-    wxBoxSizer* BoxSizer1;
-    wxGridSizer* GridSizer1;
+    wxFlexGridSizer* FlexGridSizer1;
+    wxFlexGridSizer* FlexGridSizer2;
+    wxStdDialogButtonSizer* StdDialogButtonSizer1;
 
-    Create(parent, wxID_ANY, _("Add / Edit Game"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("wxID_ANY"));
-    BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
-    Panel1 = new wxPanel(this, ID_PANEL1, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL1"));
-    GridSizer1 = new wxGridSizer(10, 2, 2, 2);
-    StaticText1 = new wxStaticText(Panel1, ID_STATICTEXT1, _("Name"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
-    GridSizer1->Add(StaticText1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    TextCtrl1 = new wxTextCtrl(Panel1, ID_TEXTCTRL1, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL1"));
-    GridSizer1->Add(TextCtrl1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    StaticText2 = new wxStaticText(Panel1, ID_STATICTEXT2, _("Path"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT2"));
-    GridSizer1->Add(StaticText2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    TextCtrl2 = new wxTextCtrl(Panel1, ID_TEXTCTRL2, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL2"));
-    GridSizer1->Add(TextCtrl2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    StaticText3 = new wxStaticText(Panel1, ID_STATICTEXT3, _("Icon Path"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT3"));
-    GridSizer1->Add(StaticText3, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    TextCtrl3 = new wxTextCtrl(Panel1, ID_TEXTCTRL3, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL3"));
-    GridSizer1->Add(TextCtrl3, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    StaticText4 = new wxStaticText(Panel1, ID_STATICTEXT4, _("Year"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT4"));
-    GridSizer1->Add(StaticText4, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    TextCtrl4 = new wxTextCtrl(Panel1, ID_TEXTCTRL4, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL4"));
-    GridSizer1->Add(TextCtrl4, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    StaticText5 = new wxStaticText(Panel1, ID_STATICTEXT5, _("Developer"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT5"));
-    GridSizer1->Add(StaticText5, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    TextCtrl5 = new wxTextCtrl(Panel1, ID_TEXTCTRL5, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL5"));
-    GridSizer1->Add(TextCtrl5, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    StaticText6 = new wxStaticText(Panel1, ID_STATICTEXT6, _("Publishers"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT6"));
-    GridSizer1->Add(StaticText6, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    TextCtrl6 = new wxTextCtrl(Panel1, ID_TEXTCTRL6, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL6"));
-    GridSizer1->Add(TextCtrl6, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    StaticText7 = new wxStaticText(Panel1, ID_STATICTEXT7, _("Genre"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT7"));
-    GridSizer1->Add(StaticText7, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    TextCtrl7 = new wxTextCtrl(Panel1, ID_TEXTCTRL7, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL7"));
-    GridSizer1->Add(TextCtrl7, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    StaticText8 = new wxStaticText(Panel1, ID_STATICTEXT8, _("Category"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT8"));
-    GridSizer1->Add(StaticText8, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    TextCtrl8 = new wxTextCtrl(Panel1, ID_TEXTCTRL8, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL8"));
-    GridSizer1->Add(TextCtrl8, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    StaticText9 = new wxStaticText(Panel1, ID_STATICTEXT9, _("Favorite\?"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT9"));
-    GridSizer1->Add(StaticText9, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    CheckBox1 = new wxCheckBox(Panel1, ID_CHECKBOX1, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_CHECKBOX1"));
-    CheckBox1->SetValue(false);
-    GridSizer1->Add(CheckBox1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    Button1 = new wxButton(Panel1, ID_BUTTON1, _("Add Game!"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON1"));
-    GridSizer1->Add(Button1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    Button2 = new wxButton(Panel1, ID_BUTTON2, _("Cancel"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
-    GridSizer1->Add(Button2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    Panel1->SetSizer(GridSizer1);
-    BoxSizer1->Add(Panel1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    SetSizer(BoxSizer1);
-    BoxSizer1->SetSizeHints(this);
+    Create(parent, wxID_ANY, _("Add / Edit Game"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER, _T("wxID_ANY"));
+    Move(wxPoint(-1,-1));
+    SetMinSize(wxSize(620,440));
+    FlexGridSizer1 = new wxFlexGridSizer(2, 1, 0, 0);
+    FlexGridSizer1->AddGrowableCol(0);
+    FlexGridSizer1->AddGrowableRow(0);
+    Notebook1 = new wxNotebook(this, ID_NOTEBOOK1, wxDefaultPosition, wxDefaultSize, 0, _T("ID_NOTEBOOK1"));
+    Notebook1->SetMinSize(wxSize(600,350));
+    Panel1 = new wxPanel(Notebook1, ID_PANEL1, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL1"));
+    FlexGridSizer2 = new wxFlexGridSizer(0, 2, 0, 0);
+    Panel1->SetSizer(FlexGridSizer2);
+    Panel2 = new wxPanel(Notebook1, ID_PANEL2, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL2"));
+    Notebook1->AddPage(Panel1, _("Launch Configuration"), false);
+    Notebook1->AddPage(Panel2, _("Info / Metadata"), false);
+    FlexGridSizer1->Add(Notebook1, 1, wxALL|wxEXPAND, 0);
+    StdDialogButtonSizer1 = new wxStdDialogButtonSizer();
+    StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_CANCEL, wxEmptyString));
+    StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_SAVE, wxEmptyString));
+    StdDialogButtonSizer1->Realize();
+    FlexGridSizer1->Add(StdDialogButtonSizer1, 1, wxALL|wxALIGN_BOTTOM|wxALIGN_CENTER_HORIZONTAL, 10);
+    SetSizer(FlexGridSizer1);
+    Fit();
     Center();
     //*)
 }
